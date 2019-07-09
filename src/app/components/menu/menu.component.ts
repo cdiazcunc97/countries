66ac1b062bd567fd8d4baf900f07706324fb48b0
Carlos Díaz Chávez <c.diaz@didepti.com>
2019-07-09 11:31:38 -0500

    arreglos

--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -12,7 +12,7 @@ export class MenuComponent implements OnInit {
 
   private titleCountry: string = 'Paises';
   private isCountries: boolean = true;
-  private flagLoadingBase = "/assets/public/imgs/loader.gif";
+  private flagLoadingBase = "/countries/assets/public/imgs/loader.gif";
   public country = '';
   public flagCountry = this.flagLoadingBase;
   public countriesDropdown: ICountryDropdown[] = [];
